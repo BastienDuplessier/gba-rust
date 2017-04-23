fbb86396594a1c063af74a8f55fa77ecf30ed354
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-04-23 23:04:14 +0200

    Display lines of colors

--- first/src/main.rs
+++ first/src/main.rs
@@ -5,11 +5,31 @@
 
 extern crate gba;
 
+use gba::gfx;
+
 mod lang;
 
 pub use lang::{__aeabi_unwind_cpp_pr0, __aeabi_unwind_cpp_pr1};
 
 #[no_mangle]
 pub extern "C" fn main(_: i32, _: *const *const i8) -> i32 {
+    gfx::Mode3::activate();
+    let colors = [gfx::Color::RED,
+                  gfx::Color::BLUE,
+                  gfx::Color::YELLOW,
+                  gfx::Color::LIME,
+                  gfx::Color::MAG,
+                  gfx::Color::WHITE,
+                  gfx::Color::CYAN,
+                  gfx::Color::BLACK];
+
+
+    for x in 0..239 {
+        let color = colors[x / 10 % 8];
+        for y in 0..159 {
+            gfx::Mode3::fill_dot(x as i32, y as i32, color);
+        }
+    }
+
     loop{}
 }
